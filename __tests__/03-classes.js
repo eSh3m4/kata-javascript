f085da4b97c92c706a2516ba6f3cb58578f8f477
eSh3m4 <n.shema.eric@gmail.com>
2020-11-02 01:46:41 +0100

    add the new classes sugar syntaxing

--- __tests__/03-classes.js
+++ __tests__/03-classes.js
@@ -5,36 +5,81 @@
   https://developer.mozilla.org/fr-FR/docs/Web/JavaScript/H%C3%A9ritage_et_cha%C3%AEne_de_prototypes
 */
 
-
 describe('about classes', () => {
-  it('1-can create an object', () => {
-    function myClass() {
-      /// complete here
-    }
-    expect(new myClass().a).toBe("a");
-  });
-  
-  it('2-can contain function', () => {
-    function myClass() {
-      var a = "a";
-      /// complete here
-    }
-    expect(new myClass().getA()).toBe("a");
-  });
-
-  it('3-can be inherited', () => {
-    function parentClass() {
-      this.getA = function () {
-        return this.a;
+  describe('about classes as functions', () => {
+    it('1-can create an object', () => {
+      function myClass() {
+        /// complete here
       }
-    }
+      expect(new myClass().a).toBe("a");
+    });
 
-    function myClass() {
-      this.a = "a";
-    }
+    it('2-can contain function', () => {
+      function myClass() {
+        var a = "a";
+        /// complete here
+      }
+      expect(new myClass().getA()).toBe("a");
+    });
 
-    myClass.prototype = _ ;
-    
-    expect(new myClass().getA()).toBe("a");
+    it('3-can be inherited', () => {
+      function parentClass() {
+        this.getA = function () {
+          return this.a;
+        }
+      }
+
+      function myClass() {
+        this.a = "a";
+      }
+
+      myClass.prototype = _ ;
+
+      expect(new myClass().getA()).toBe("a");
+    });
+  });
+
+  describe('about modern classes', () => {
+    it('4-can create an object', () => {
+      class myClass {
+        constructor() {
+          /// complete here
+        }
+      }
+      expect(new myClass().a).toBe("a");
+    });
+
+    it('5-can contain getters', () => {
+      class myClass {
+        constructor() {}
+        /// complete here
+      }
+      expect(new myClass().a).toBe("a");
+    });
+
+    it('6-can contain static methods', () => {
+      class myClass {
+        constructor() {}
+        /// complete here
+      }
+      expect(myClass.getA()).toBe("a");
+      expect(new myClass().getA).toBe( _ );
+    });
+
+    it('7-can be inherited', () => {
+      class parentClass {
+        constructor() {
+          this.a = 'a';
+        }
+      }
+
+      class myClass extends _ {
+        constructor() {
+          _ ;
+        }
+      }
+
+      expect(new myClass().a).toBe("a");
+    });
   });
 });
